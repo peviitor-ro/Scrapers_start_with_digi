13a2afd855cd14de8067cd8981d683da87725268
andreireporter13 <andrei.reporter13@gmail.com>
2023-05-07 23:58:54 +0300

    add agilefreaks

---
+++ scrapers_forzza/agilefreaks_scraper.py
@@ -0,0 +1,54 @@
+#
+#
+#
+# New Scraper For ---> agilefreaks
+# Link to this Company ---> https://careers.agilefreaks.com/jobs
+#
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
+#
+import requests
+from bs4 import BeautifulSoup
+#
+import uuid
+
+
+def collect_data_from_site():
+    """
+    Collect data from https://careers.agilefreaks.com/jobs
+    """
+
+    response = requests.get('https://careers.agilefreaks.com/jobs', headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    soup_data = soup.find_all('li', class_='w-full')
+
+    lst_wit_data = []
+    for sd in soup_data:
+        link = sd.find('a')['href'].strip()
+        title = sd.find('span', class_='text-block-base-link sm:min-w-[25%] sm:truncate company-link-style').text
+
+        lst_wit_data.append({
+            "id": str(uuid.uuid4()),
+            "job_title": title,
+            "job_link":  link,
+            "company": "agilefreaks",
+            "country": "Romania",
+            "city": "Romania"
+            })
+
+    return lst_wit_data
+
+
+# update data on peviitor!
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'agilefreaks'
+data_list = collect_data_from_site()
+scrape_and_update_peviitor(company_name, data_list)
