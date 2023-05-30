b027ac186914a8ab695f1f1dbbe8ca0c0067530c
andreireporter13 <andrei.reporter13@gmail.com>
2023-05-31 01:20:40 +0300

    repair suvoda, partial

--- scrapers_forzza/suvoda_scraper.py
+++ scrapers_forzza/suvoda_scraper.py
@@ -21,18 +21,18 @@ def special_headers():
 
     headers = {
         'authority': 'www.suvoda.com',
-        'accept': 'application/json, text/plain, */*',
+        #'accept': 'application/json, text/plain, */*',
         'accept-language': 'en-US,en;q=0.9',
-        'cookie': '__cfruid=270f54863cc6f05f871b72548d9bd41f223194dc-1683471130; __cf_bm=oRBuLiQx10YKu56b1eX3u6CqxfpCMyHRJ2erjv6uds4-1683473698-0-AcDhd2Ey24p6vd5U9398ovnvbOCkOffYQt9JwoLtK0lo59rrAXD+uOidgcfMce+zbI5AHRlTQwsfKVolrt4Blic=',
-        'referer': 'https://www.suvoda.com/careers/job-openings',
+        #'cookie': '__cfruid=270f54863cc6f05f871b72548d9bd41f223194dc-1683471130; __cf_bm=oRBuLiQx10YKu56b1eX3u6CqxfpCMyHRJ2erjv6uds4-1683473698-0-AcDhd2Ey24p6vd5U9398ovnvbOCkOffYQt9JwoLtK0lo59rrAXD+uOidgcfMce+zbI5AHRlTQwsfKVolrt4Blic=',
+        #'referer': 'https://www.suvoda.com/careers/job-openings',
         'sec-ch-ua': '"Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
+        #'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Linux"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
         'sec-gpc': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
       }
 
     return headers
@@ -49,7 +49,7 @@ def collect_data_from_suvoda():
 
     lst_with_data = []
     for dt in soup.find_all('div', class_='opening'):
-        if 'Romania' in str(dt.find('span', class_='location')):
+        if 'Romania' in str(dt.find('span', class_='location')) or 'remote' in dt.find('a').text.lower():
             link = dt.find('a')['href']
             title = dt.find('a').text
 
