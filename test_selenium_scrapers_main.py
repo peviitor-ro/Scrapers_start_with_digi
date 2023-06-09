b84d1213ffe1d86963929f28cbc749f0560131d6
andreireporter13 <andrei.reporter13@gmail.com>
2023-06-09 22:17:18 +0300

    changes

--- test_selenium_scrapers_main.py
+++ test_selenium_scrapers_main.py
@@ -4,11 +4,11 @@
 # ---> Run all scraper here1
 #
 # import config driver here!
-from browser_configured_py import chromedriver_config
+from browser_test_scraper import chromedriver_config
 #
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# ---> Test scrapers here!
+# ---> Test scrapers here
