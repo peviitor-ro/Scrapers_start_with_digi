fc116836aab0c6fa6d58e457cacabec5c29ae85c
andreireporter13 <andrei.reporter13@gmail.com>
2023-06-12 00:30:23 +0300

    add back rcsrds and refactor project

--- scrapers_forzza/rcsrds_scraper.py
+++ scrapers_forzza/rcsrds_scraper.py
@@ -3,34 +3,16 @@
 #
 # Scraper for digi.ro ---> Cariere.
 #
+from A_OO_get_post_soup_update_dec import update_peviitor_api
+from L_00_logo import update_logo
+#
 import requests
 from bs4 import BeautifulSoup
+#
 import re
-import json
 import uuid
-from random import randint
+#
 import time
-
-
-# start count time
-start_time = time.time()
-
-
-def split_special_name(name: str) -> str:
-    """ func() to convert to normal string for requests.
-    """
-    print(f" Jobul {name} ---> scrapuit.")
-    if ',' in name:
-        name = name.replace(',', '%2C')
-        new_name = "+".join(name.split())
-        return new_name.strip()
-
-    elif name and len(name.split()) > 1:
-        new_name_2 = "+".join(name.split())
-        return new_name_2.strip()
-
-    else:
-        return name
 
 
 # headers for post requests
@@ -42,29 +24,29 @@ def get_post_data(city: str, job_title: str) -> tuple:
     url = 'https://www.digi.ro/cariere/search-xhr'
 
     headers = {
-        'authority': 'www.digi.ro',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.6',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': 'DGROSESSV3PRI=BAF20A5FBB184D59BDF54256B7C5B2EE8F75B2552801437C; webpush=0; cmp_level=15',
-        'origin': 'https://www.digi.ro',
-        'referer': 'https://www.digi.ro/cariere',
-        'sec-ch-ua': '"Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'sec-gpc': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-        'x-requested-with': 'XMLHttpRequest'
-    }
+            'authority': 'www.digi.ro',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.5',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'cookie': 'DGROSESSV3PRI=E2D7096834AD414697BF27A5EAAE0C859F3643EA81874F9B; webpush=0; cmp_level=15',
+            'origin': 'https://www.digi.ro',
+            'referer': 'https://www.digi.ro/cariere',
+            'sec-ch-ua': '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest'
+        }
 
     data = {
         'action': 'assistanceSupportCareers',
         'company': 'rcs-rds',
         'county':  city,
-        'industry': split_special_name(job_title)
+        'industry': job_title
     }
 
     return url, headers, data
@@ -110,11 +92,16 @@ def scrape_data_from_digi(cities: list, jobs: list, session) -> list:
             returned_head = get_post_data(city=city, job_title=job)
             response = session.post(url=returned_head[0], headers=returned_head[1], data=returned_head[2])
 
-            soup = BeautifulSoup(response.content, 'lxml')
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            job_titles = re.findall(r'<label class="accordion-title"[^>]*>([^<]+)<', str(soup))
+            job_links = re.findall(r'href="([^"]+)"', str(soup))
+
+            #print(job_links, job_titles)
 
             # data from post requests in list
-            job_titles = [tag.text for tag in soup.find_all(attrs={"class": "accordion-title"})]
-            job_links = [links.a['href'] for links in soup.find_all(attrs={"class": "align-right"})]
+            #job_titles = [tag.text for tag in soup.find_all('label', class_='accordion-title')]
+            #job_links = [links['href'] for links in soup.find_all('a', class_='btn-round-right')]
 
             if (len(job_titles) == len(job_links)) and (len(job_titles) + len(job_links)) > 0:
                 for id in range(len(job_titles)):
@@ -131,28 +118,7 @@ def scrape_data_from_digi(cities: list, jobs: list, session) -> list:
                             "city": city
                             })
 
-                    print(f"Jobul {job} ---> a fost scrapuit")
-            print(f"Scraping on job ---> in {city}")
-            time.sleep(randint(1, 2))
-        print("=================================")
-
-    # sorted results
-    new_lst = []
-    for diction in lst_with_jobs:
-        new_lst.append(diction['job_link'])
-
-    returned_list = []
-    unic_links = list(set(new_lst))
-    for unic_link in unic_links:
-        for data_dict in lst_with_jobs:
-            if unic_link == data_dict['job_link']:
-                returned_list.append(data_dict)
-
-                break
-
-    print(len(returned_list))
-
-    return returned_list
+    return lst_with_jobs
 
 
 def rcsrds_scrape():
@@ -164,8 +130,23 @@ def rcsrds_scrape():
 
     lst_with_data_post = scrape_data_from_digi(cities=tuple_jobs_cities[0], jobs=tuple_jobs_cities[1], session=session)
 
-    with open('scrapers_forzza/data_digi.json', 'w') as file:
-        json.dump(lst_with_data_post, file)
+    return lst_with_data_post
 
-    finish_time = time.time() - start_time
-    print(f"Script execute in {int(finish_time) / 60}")
+
+# update data on peviitor!
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'rcsrds'
+data_list = rcsrds_scrape()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('rcsrds',
+                  'https://www.digi.ro/static/theme-ui-frontend/bin/images/logo-digi-alt.png'
+                  ))
