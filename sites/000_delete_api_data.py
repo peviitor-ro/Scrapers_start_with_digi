bd8eaefbce97d8221a90ed17bd5aeaee177d33ed
andreireporter13 <andrei.reporter13@gmail.com>
2023-06-18 13:27:18 +0300

    refactor scraper_forrza in sites

---
+++ sites/000_delete_api_data.py
@@ -0,0 +1,24 @@
+#
+#
+#
+#
+import requests
+import os
+
+def clean_data(company_name: str) -> None:
+
+    API_KEY = os.environ.get('API_KEY')
+    CLEAN_URL = 'https://api.peviitor.ro/v4/clean/'
+
+    print('API_KEY = ', API_KEY)
+
+    clean_header = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'apikey': API_KEY
+        }
+
+    clean_request = requests.post(CLEAN_URL, headers=clean_header, data={'company': company_name})
+    print(f"{company_name} clean -> {clean_request.status_code}")
+
+
+print(clean_data('globallogic'))
