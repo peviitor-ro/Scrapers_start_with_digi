256cdc2961e5b97081d3ea5e142f5d0f9d6f3e95
andreireporter13 <andrei.reporter13@gmail.com>
2023-07-02 03:13:05 +0300

    add partial scraper connect44

--- sites/connect44_scraper.py
+++ sites/connect44_scraper.py
@@ -11,31 +11,53 @@ import requests
 from bs4 import BeautifulSoup
 #
 import uuid
+import re
 
 
 # session for new session!
 session = requests.Session()
 
 
-def csrf_and_session_tokens() -> str:
+def csrf_and_session_tokens() -> tuple[str]:
     '''
     '''
 
-    response = requests.get(url='https://www.connect44.com/careers/jobs?country=3&search=',
+    response = session.get(url='https://www.connect44.com/careers/jobs?country=3&search=',
                             headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
 
     # token id and session id
     tokens = response.headers
+    x_csrf = re.search(r'XSRF-TOKEN=([^;]+)', str(tokens)).group(0)
+    connec44_id = re.search(r'connect44_session=([^;]+)', str(tokens)).group(0)
+
+    # check sum
+    div_elements = soup.find_all('div', attrs={'wire:id': True})
+    wire_ids = [div['wire:id'] for div in div_elements]
+
+    # htmlHash
+    html_hash_regex = r'htmlHash":"([^"]+)'
+    html_hash_match = re.search(html_hash_regex, str(div_elements))
+    if html_hash_match:
+        html_hash = html_hash_match.group(1)
+
+    # v Value!
+    v_regex = r'"v":"([^"]+)'
+    v_match = re.search(v_regex, str(div_elements))
+    if v_match:
+        v_value = v_match.group(1)
+
+    # search for checksum
+    regex = r'checksum":"([^"]+)"'
+    matches = re.findall(regex, str(div_elements))
+    if matches:
+        checksum = matches[0]
 
     # catch csrf token
     meta_csrf = soup.find_all('meta')
     csrf = meta_csrf[-1]['content']
 
-    return csrf, tokens
-
-
-print(csrf_and_session_tokens())
+    return csrf, x_csrf, connec44_id, wire_ids[0], checksum, html_hash, v_value
 
 
 def prepare_post_request() -> tuple:
@@ -43,22 +65,86 @@ def prepare_post_request() -> tuple:
     ... here, prepare url, headers and data for post request.
     '''
 
+    data = csrf_and_session_tokens()
+
     url = 'https://www.connect44.com/livewire/message/jobs-table',
 
     headers = {
+
             'Accept': 'text/html, application/xhtml+xml',
-          -H 'Accept-Language: en-US,en;q=0.5' \
-          -H 'Connection: keep-alive' \
-          -H 'Content-Type: application/json' \
-          -H 'Cookie: CookieConsent={stamp:%27ND9DqhKFmppbOWaxrzR7/vmb82Rdt/kSiCvVQz3k06hyIo3E+8UlHA==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1688208124784%2Cregion:%27ro%27}; XSRF-TOKEN=eyJpdiI6IllxekcxZHpCNHpUUEtROTY0Z3RVcUE9PSIsInZhbHVlIjoiSWxLcGgxZjVwYmd0Snc4TzFyekRjV1lYSHpaS2o4TzJ3NjBjeGExazYwOFNlQ0pUZVZaWTBtejZFTnZhdThlaGpsd1F3MlVkZU9ndnRvUzNMa2szcXBsQVFvd3I5dFRraW1QWkV6Sk1vdmtqNVdzQ2gzUWlReUlaU044RlZTVkYiLCJtYWMiOiJiYzliYjRlYTAxYWEwZTFhMjViOTU2NDFkMzM2ODA3YTA5OTQ1ZmE0MTc3MzQ5NGYyMTYyMWU1NjJhNTlmZDk1IiwidGFnIjoiIn0%3D; connect44_session=eyJpdiI6InR0ZUVYL1FTUGpmNFdZWVJ2dEwveXc9PSIsInZhbHVlIjoia0tWZ3dJNFRJRU1VNHhaUjgzL1N4eTNRTnhVazFtVlJDUUM2VFZUWDJMbHhDN0NMaW1QUmZ3R3JOb1lpSTFybnV5RUtsc1I0VE5qV2FkY2Yybmh6NnkyZHUrTGNJSFhXL2hraWlGblZCcXVKdVZvNTZpOVM3ZWQ2aitIOVl2eGIiLCJtYWMiOiJiY2RkMWE0ZDQ4OTdmZGFlZDgwZDBhNGUzNjUwNTdjNjdmMGI2Yjg3ZmM1NmUwZDZjMzZjZDAwYjQ2MjNhNzg4IiwidGFnIjoiIn0%3D' \
-          -H 'Origin: https://www.connect44.com' \
-          -H 'Referer: https://www.connect44.com/careers/jobs?country=3&city=' \
-          -H 'Sec-Fetch-Dest: empty' \
-          -H 'Sec-Fetch-Mode: cors' \
-          -H 'Sec-Fetch-Site: same-origin' \
-          -H 'Sec-GPC: 1' \
-          -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
-          -H 'X-CSRF-TOKEN: 3hIoEcyUENUf9j4cetdQavVmRU4wrZdPBdg2hzhF' \
-          -H 'X-Livewire: true' \
-  --data-raw '{"fingerprint":{"id":"I0zMkCYpbLINzhd23wYa","name":"jobs-table","locale":"en","path":"careers/jobs","method":"GET","v":"acj"},"serverMemo":{"children":[],"errors":[],"htmlHash":"16ff79d8","data":{"perPage":6,"search":null,"query":null,"country":"3","city":"","cities":[]},"dataMeta":{"modelCollections":{"cities":{"class":"App\\Models\\JobsCountryCity","id":[20,53],"relations":[],"connection":"tenant","collectionClass":null}}},"checksum":"ea7d114ce5e1d48ea71d61e4d02e257689cb46a37e96495192d62fe56b20266f"},"updates":[{"type":"callMethod","payload":{"id":"1zgk","method":"loadMore","params":[]}}]}' \
-  --compressed
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Cookie': f'CookieConsent={"stamp:%27ND9DqhKFmppbOWaxrzR7/vmb82Rdt/kSiCvVQz3k06hyIo3E+8UlHA==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1688208124784%2Cregion:%27ro%27"}; {data[1]}; {data[2]};',
+            'Origin': 'https://www.connect44.com',
+            'Referer': 'https://www.connect44.com/careers/jobs?country=3&city=',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-GPC': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            'X-CSRF-TOKEN': f'{data[0]}',
+            'X-Livewire': 'true'
+            }
+
+    json_data = {
+            'fingerprint': {
+                'id': f'{data[3]}',
+                'name': 'jobs-table',
+                'locale': 'en',
+                'path': 'careers/jobs',
+                'method': 'GET',
+                'v': f'{data[6]}'
+            },
+            'serverMemo': {
+                'children': [],
+                'errors': [],
+                'htmlHash': f'{data[5]}',
+                'data': {
+                    'perPage': 6,
+                    'search': None,
+                    'query': None,
+                    'country': '3',
+                    'city': '',
+                    'cities': []
+                },
+                'dataMeta': {
+                    'modelCollections': {
+                        'cities': {
+                            'class': 'App\\Models\\JobsCountryCity',
+                            'id': [20, 53],
+                            'relations': [],
+                            'connection': 'tenant',
+                            'collectionClass': None
+                        }
+                    }
+                },
+                'checksum': f'{data[5]}'
+            },
+            'updates': [
+                {
+                    'type': 'callMethod',
+                    'payload': {
+                        'id': '1zgk',
+                        'method': 'loadMore',
+                        'params': []
+                    }
+                }
+            ]
+        }
+
+    return url, headers, json_data
+
+
+def collect_data_from_connect44() -> list[dict]:
+    '''
+    ... collect data with prepared headers.
+    '''
+
+    post_data = prepare_post_request()
+
+    response = session.post(url=post_data[0], headers=post_data[1], json=post_data[2])
+    print(response)
+
+
+print(collect_data_from_connect44())
