705b8bc42699a3d5c9f71b3dabf0e912aad62624
andreireporter13 <andrei.reporter13@gmail.com>
2023-08-06 01:11:11 +0300

    change fresh cookie every time in osf scraper

--- sites/osf_scraper.py
+++ sites/osf_scraper.py
@@ -4,49 +4,65 @@
 # Scraper for OSF Digital Company
 # Link to ---> https://osf.digital/careers/jobs?location=romania
 #
-from A_OO_get_post_soup_update_dec import update_peviitor_api
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
 from L_00_logo import update_logo
 #
 import requests
 from bs4 import BeautifulSoup
 #
 import uuid
+import re
+
+
+# this is instance for session. Scraper need this because
+# ... it use fresh cookie evrey time
+session = requests.Session()
+
+
+def get_ids() -> tuple:
+    '''
+    ... return fresh cookie data from OSF.
+    '''
+
+    res = session.head(url='https://osf.digital/careers/jobs?location=romania',
+                       headers=DEFAULT_HEADERS).headers
+
+    # search cookie by regex
+    return re.search(r"__RequestVerificationToken=([A-Za-z0-9_-]+);", str(res)).group(0)
 
 
 def set_headers() -> tuple:
     """
     Set default headers for scraping.
     """
+    token_cookie = get_ids()
 
     url = 'https://osf.digital/careers/jobs?location=romania'
 
     headers = {
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Cookie': '__RequestVerificationToken=kpTO_9WZW3pOy75NA-0BR6W7mqJpyqlfoK8W-1VPCGo4gASGHh2Z_Knp31rwbf0BgE3TM-xDcTuaMn6T7YfWqLNAyt41; ASP.NET_SessionId=dwkpwrwqwhxqpnhisacucsr2; CookieConsent={stamp:%27x7uXJqRZohYFzf3eRobuXgRXFC9Mqs0zpkQIUIMidEc+uzuQysCDvw==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1684179516211%2Cregion:%27ro%27}; SC_ANALYTICS_GLOBAL_COOKIE=33fa9e79dfbb416699b3b7d558be6305|True',
-            'Origin': 'https://osf.digital',
-            'Referer': 'https://osf.digital/careers/jobs?location=romania',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-GPC': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-            'X-Requested-With': 'XMLHttpRequest',
-            'sec-ch-ua': '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"'
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': '\'' + token_cookie + ' CookieConsent={stamp:%27gF2wOFqMFWAwPXKKsC7/bA566Qm6ilqaFpgbB03SHW1hzoOyQCLpqA==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1691269536897%2Cregion:%27ro%27}',
+        'Origin': 'https://osf.digital',
+        'Referer': 'https://osf.digital/careers/jobs?location=romania',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-GPC': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
         }
 
-    query_params = {
-            "scController": "OsfCommerceJob",
-            "scAction": "GetItems",
-            "parameter": "request",
-            "__RequestVerificationToken": "NyvePAz4BlExbdD3T6Cy_unJv67n9mfU003xcPtjajGVf5lOKsGBJrXRvIjbFdWZnBsH7IfIsj6Y-8vLecm3_EyiTZ81"
+    data = {
+        'scController': 'OsfCommerceJob',
+        'scAction': 'GetItems',
+        'parameter': 'request',
+        '__RequestVerificationToken': f'{token_cookie.split("=")[1][:-1]}'
         }
 
-    return url, headers, query_params
+    return url, headers, data
 
 
 def collect_data_osf() -> list:
@@ -54,7 +70,7 @@ def collect_data_osf() -> list:
     Collect all data with session. OSF!
     """
     data = set_headers()
-    response = requests.post(
+    response = session.post(
             url=data[0],
             headers=data[1],
             data=data[2]
@@ -95,5 +111,4 @@ data_list = collect_data_osf()
 scrape_and_update_peviitor(company_name, data_list)
 
 print(update_logo('osf',
-                  'https://osf.digital/library/media/osf/digital/common/header/osf_digital_logo.svg?h=60&la=en&w=366&hash=5FF21BA406E10D94D9778FA8A3A8AEC43C247D2B'
-                  ))
+                  'https://osf.digital/library/media/osf/digital/common/header/osf_digital_logo.svg?h=60&la=en&w=366&hash=5FF21BA406E10D94D9778FA8A3A8AEC43C247D2B'))
