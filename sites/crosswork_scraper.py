392d374fe47d32adc974b2437e38340a0c4cf7d4
andrei.reporter13@gmail.com <andrei.reporter13@gmail.com>
2023-08-21 21:49:12 +0300

    added little change crosswork

--- sites/crosswork_scraper.py
+++ sites/crosswork_scraper.py
@@ -3,7 +3,7 @@
 #
 # Company - crosswork
 # Link -> https://crosswork.sincron.biz/
-#
+# ---> work...
 from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
 from L_00_logo import update_logo
 #
