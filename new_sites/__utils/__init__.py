e5b44f9c94f4abb41c39bc05366d9bd2e93b2119
andreireporter13 <andrei.reporter13@gmail.com>
2023-11-26 20:01:04 +0200

    delete 2 scrapers add 1

--- new_sites/__utils/__init__.py
+++ new_sites/__utils/__init__.py
@@ -13,7 +13,6 @@ from __utils.req_bs4_shorts import(
     GetHtmlSoup,
     GetHeadersDict,
 )
-from __utils.req_bs4_shorts import GetHtmlSoup
 from __utils.dynamic_requests_html_shorts import GetDynamicSoup
 from __utils.get_job_type import get_job_type
 from __utils.get_data_with_regex import get_data_with_regex
---
+++ new_sites/canamgroup_scraper.py
@@ -0,0 +1,124 @@
+#
+#
+#  Basic for scraping data from static pages
+#
+# ------ IMPORTANT! ------
+# if you need return soup object:
+# you cand import from __utils -> GetHtmlSoup
+# if you need return regex object:
+# you cand import from __utils ->
+# ---> get_data_with_regex(expression: str, object: str)
+#
+# Company ---> CanamGroup
+# Link ------> https://www.canam.com/en/job-opportunities/?country%5B%5D=romania&search=
+#
+#
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+'''
+    Daca te-ai deprins cu aceasta formula de cod,
+    atunci poti sterge acest comentariu din fisierul
+    __create_scraper.py, din functia -> create_static_scraper_config <-
+
+    Deci:
+    ########################################################################
+    1) --->  clasa GetStaticSoup returneaza un obiect BeautifulSoup,
+    direct in instanta, fara a apela alte metode.
+
+    soup = GetStaticSoup(link) -> si gata, ai acces la obiectul soup
+    si deja poti face -> for job in soup.find_all(...)
+
+    + poti sa-i adaugi si custom_headers
+    soup = GetStaticSoup(link, custom_headers)
+    ... by default, custom_headers = None, dar in __utils ai un fisier
+    default_headers.py unde poti sa-ti setezi headerele tale default.
+
+    --------------IMPORTANT----------------
+    La nivel de proiect, ca o variabila globala, este definit Session()!
+    ... acest session inseamna ca orice clasa va putea folosi
+    ... aceeasi sesiune, practic se va evita multiple requests;
+
+    ########################################################################
+
+    2) ---> get_county(nume_localitate) -> returneaza numele judetului;
+    poti pune chiar si judetul, de exemplu, nu va fi o eroare.
+
+    ########################################################################
+
+    3) --->get_job_type(job_type: str) -> returneaza job_type-ul: remote,
+    hybrid, on-site
+
+    ########################################################################
+
+    4) ---> Item -> este un struct pentru datele pe care le vom stoca in lista
+    si, apoi, le vom trimite catre API.
+    exemplu: job_list.append(Item(job_title="titlu_str",
+                                    job_link="link",
+                                    company="nume_companie",
+                                    country="Romania",
+                                    county="Judetul",
+                                    city="Orasul",
+                                    remote="remote, onsite sau hibryd"))
+
+    ########################################################################
+
+    5) ---> clasa UpdateAPI are doua metode:
+    update_jobs(lista_dict_joburi) si update_logo(nume_companie, link_logo)
+
+    UpdateAPI().update_jobs(company_name: str, data_jobs: list)
+    UpdateAPI().update_logo(id_company: str, logo_link: str)
+
+    ########################################################################
+'''
+
+
+def scraper():
+    '''
+    ... scrape data from CanamGroup scraper.
+    '''
+    soup = GetStaticSoup("https://www.canam.com/en/job-opportunities/?country%5B%5D=romania&search=")
+
+    job_list = []
+    for job in soup.find_all('a', attrs={'class': 'c-card-job'}):
+        location = job.find('div', attrs={'class': 'c-btn c-btn--ghost c-btn--tag u-pointer-events-none'}).text.strip()
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title=job.find('div', attrs={'class': 'c-card-job__title u-heading-600'}).find('span').text,
+            job_link=job.get('href'),
+            company='CanamGroup',
+            country='Romania',
+            county=get_county(location),
+            city=location,
+            remote=get_job_type(location),
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+    '''
+    ... Main:
+    ---> call scraper()
+    ---> update_jobs() and update_logo()
+    '''
+
+    company_name = "CanamGroup"
+    logo_link = "logo_link"
+
+    jobs = scraper()
+
+    # uncomment if your scraper done
+    #UpdateAPI().update_jobs(company_name, jobs)
+    #UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
