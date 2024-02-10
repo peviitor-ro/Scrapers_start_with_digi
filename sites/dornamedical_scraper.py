7d04686b65f88b0efbc7088085f1d419eae82cbc
andreireporter13 <andrei.reporter13@gmail.com>
2024-02-11 00:48:16 +0200

    added dornamedical in new build

---
+++ sites/dornamedical_scraper.py
@@ -0,0 +1,74 @@
+#
+#
+#  Basic for scraping data from static pages
+#
+# ------ IMPORTANT! ------
+# if you need return soup object:
+# you cand import from __utils -> GetHtmlSoup
+# if you need return regex object:
+# you cand import from __utils ->
+# ---> get_data_with_regex(expression: str, object: str)
+#
+# Company ---> DornaMedical
+# Link ------> https://www.dornamedical.ro/cariere/
+#
+#
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+def scraper():
+    '''
+    ... scrape data from DornaMedical scraper.
+    '''
+    soup = GetStaticSoup("https://www.dornamedical.ro/cariere/")
+
+    job_list = []
+    for job in soup.find_all('div', attrs={'class': 'single_job'}):
+
+        # get all locations
+        locations = [element.strip() for element in
+                    job.find('div', attrs={'class': 'location'}).text.strip().split(',')
+                    if element.strip()]
+        
+        # get counties ---> ned for ternar operator
+        job_counties = [get_county(city_ro) for city_ro in locations]
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title=job.find('h3').text.strip(),
+            job_link=job.find('a')['href'].strip(),
+            company='DornaMedical',
+            country='Romania',
+            county=job_counties[0] if len(locations) == 1 else job_counties,
+            city=locations[0] if len(locations) == 1 else locations,
+            remote='on-site',
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+    '''
+    ... Main:
+    ---> call scraper()
+    ---> update_jobs() and update_logo()
+    '''
+
+    company_name = "DornaMedical"
+    logo_link = "https://www.dornamedical.ro/wp-content/uploads/2022/04/logo-2-culori.png"
+
+    jobs = scraper()
+
+    # uncomment if your scraper done
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
