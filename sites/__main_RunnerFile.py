4e582333893eb1283d5c5abc8e594a2e8db45bf4
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-03-14 01:56:14 +0200

    Update .gitignore and refactor scraper execution in __main_RunnerFile.py; change Peviitor API token URL in peviitor_update.py

--- sites/__main_RunnerFile.py
+++ sites/__main_RunnerFile.py
@@ -1,26 +1,45 @@
-#
-#
-#
-#
-#
-#
+
 import os
 import subprocess
 
-# exclude files
-exclude = ['__init__.py',
-           '__create_scraper.py',
-           '__main_RunnerFile.py',
-           ]
 
-path = os.path.dirname(os.path.abspath(__file__))
+class Scraper:
+    def __init__(self, exclude):
+        """
+        Initialize the Scraper class with a list of files to exclude.
+        """
+        self.exclude = exclude
 
-for site in os.listdir(path):
-    if site.endswith('.py') and site not in exclude:
-        action = subprocess.run(['python', os.path.join(path, site)], capture_output=True)
-        if action.returncode != 0:
-            errors = action.stderr.decode('utf-8')
-            print("Error in " + site)
-            print(errors)
-        else:
-            print("Success scraping " + site)
+    def run(self):
+        """
+        Run the scraper for each Python file in the current directory, excluding the specified files.
+        """
+        path = os.path.dirname(os.path.abspath(__file__))
+
+        for site in os.listdir(path):
+            if site.endswith('.py') and site not in self.exclude:
+                script_path = os.path.join(path, site)
+                try:
+                    print(f"Running: python3 {script_path}")
+                    # Run the script with 'python3' using shell=True to simulate direct execution
+                    result = subprocess.run(
+                        f'python3 "{script_path}"', cwd=path, shell=True, check=True, env=os.environ)
+                    print(
+                        f"Success scraping {site} with exit code {result.returncode}")
+                except subprocess.CalledProcessError as e:
+                    print(
+                        f"Error scraping {site} with exit code {e.returncode}: {e}")
+                except Exception as e:
+                    print(
+                        f"An unexpected error occurred while scraping {site}: {e}")
+
+
+if __name__ == "__main__":
+    # exclude files
+    exclude = ['__init__.py',
+               '__create_scraper.py',
+               '__main_RunnerFile.py',
+               ]
+
+    scraper = Scraper(exclude)
+    scraper.run()
--- sites/__utils/peviitor_update.py
+++ sites/__utils/peviitor_update.py
@@ -45,7 +45,8 @@ class UpdateAPI:
         'User-Agent': UA.random,
         }
 
-        self.access_token = requests.request("POST", "https://api.peviitor.ro/v5/get_token/", headers=post_header, data=payload).json()['access']
+        self.access_token = requests.request(
+            "POST", "https://api.laurentiumarian.ro/get_token", headers=post_header, data=payload).json()['access']
         print(self.access_token)
 
 
